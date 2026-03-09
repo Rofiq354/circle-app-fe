00daa0521325f7167cfae69a459ea0fe84c80061
Rofiq354 <rofik010206@gmail.com>
2026-03-09 23:11:52 +0700

    feat: .\Dockerfile in project and fix: api

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${import.meta.env.BACK_END_URL}/api/v1`,
+  baseURL: `${import.meta.env.VITE_BACK_END_URL}/api/v1`,
   withCredentials: true,
 });
 
